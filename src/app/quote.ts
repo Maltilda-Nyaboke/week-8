c8b01a9b274ba1cb31bea23d09e959aea4494bce
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-04-09 15:35:08 +0300

    style:add constructor

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,4 +1,9 @@
 export class Quote {
+    quote: string;
+    author:string;
+    upvotes:number;
+    downvotes:number
+
     constructor(quote: string,author:string,upvotes:number,downvotes:number){
       this.quote = quote;
       this.author =author;
