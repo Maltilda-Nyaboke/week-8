3d99708ff3fbbd18a2f1f5c186641950b63ae4e5
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-04-09 14:50:22 +0300

    style add elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteHighlightDirective } from './quote-highlight.directive';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { QuoteFormComponent } from './quote-form/quote-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,4 +1,9 @@
 export class Quote {
-    quote: string;
-    author:string;
+    constructor(quote: string,author:string,upvotes:number,downvotes:number){
+      this.quote = quote;
+      this.author =author;
+      this.upvotes = upvotes;
+      this.downvotes = downvotes;  
+    }
+
 }
