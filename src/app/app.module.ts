3ca1b71420e15a1f3bb6d469e57ff270eb227b50
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-04-09 12:36:39 +0300

    style:add boilerplate

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { QuoteHighlightDirective } from './quote-highlight.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuoteHighlightDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
