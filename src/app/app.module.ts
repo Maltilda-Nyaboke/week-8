872d498abf4a3cc3a938ef27c0ef8ed35bdbb3d4
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-04-09 13:09:53 +0300

    feature:add form-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteHighlightDirective } from './quote-highlight.directive';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteHighlightDirective
+    QuoteHighlightDirective,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
