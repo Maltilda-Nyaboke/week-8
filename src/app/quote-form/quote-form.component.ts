5a891bc19fb2b0594bd9eba00e987d1756a09bd6
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-04-11 20:06:46 +0300

    style:add elements

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { QuoteDetailComponent } from '../quote-detail/quote-detail.component';
 import { Quote } from '../quote';
 
+
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
@@ -21,13 +22,19 @@ newQuote = new Quote('','','', 0 , 0,new Date( ));
 
   author ="";
   quotes:Quote[]=[
-    new Quote('“When you have a dream, you’ve got to grab it and never let go.”','Carol Burnett','Maltilda Nyaboke', 0, 0,new Date(12,3,2020)),
-    new Quote('“Nothing is impossible. The word itself says ‘I’m possible!”','Audrey Hepburn','Maltilda Nyaboke', 0, 0,new Date(12,3,2020)),
-    new Quote('“You define your own life. Don’t let other people write your script.”','Oprah Winfrey','Maltilda Nyaboke', 0,0,new Date(12,3,2020)),
-    new Quote('“Spread love everywhere you go.”','Mother Teresa','Maltilda Nyaboke', 0, 0,new Date(12,3,2020))
+    new Quote('“When you have a dream, you’ve got to grab it and never let go.”','Carol Burnett','Maltilda Nyaboke', 0, 0,new Date(2006,13,2020)),
+    new Quote('“Nothing is impossible. The word itself says ‘I’m possible!”','Audrey Hepburn','Maltilda Nyaboke', 0, 0,new Date(2007,6,2020)),
+    new Quote('“You define your own life. Don’t let other people write your script.”','Oprah Winfrey','Maltilda Nyaboke', 0,0,new Date(2012,3,2030)),
+    new Quote('“Spread love everywhere you go.”','Mother Teresa','Maltilda Nyaboke', 0, 0,new Date(2013,3,2020))
   ]
-create(){
+create(quote){
+  let quo= quote.quote;
+  let aut = quote.author;
+  let own = quote.owner;
+  let pos = quote.postDate;
   let quoteLength = this.quotes.length;
+  // console.log(quote);
+  
   this.quotes.push(this.newQuote)
 }
 
